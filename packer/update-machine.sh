e489181825e1a24b8c3aa63282457907444645e0
vishalnvora <vishalnvora@hotmail.com>
2021-01-05 01:00:20 +0530

    Update update-machine.sh

--- packer/update-machine.sh
+++ packer/update-machine.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-
 sudo rm /boot/grub/menu.lst
 sudo apt upgrade -y
 sudo apt install -y software-properties-common git python-dev htop ntp jq apt-transport-https
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt update -y 
 sudo apt install python3.8
-sudo python ––version
+
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
 wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
+python3 get-pip.py
 rm get-pip.py
 pip install boto awscli
