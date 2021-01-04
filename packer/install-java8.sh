d1d0f8519dd42046ffd966a452625deeb9915fc1
vishalnvora <vishalnvora@hotmail.com>
2021-01-04 23:39:48 +0530

    Update install-java8.sh

--- packer/install-java8.sh
+++ packer/install-java8.sh
@@ -3,15 +3,16 @@
 # Add the Java 8 repository.
 # sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
 # sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
-sudo sudo add-apt-repository ppa:webupd8team/java
-# sudo sudoapt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+# sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+# sudo sudo add-apt-repository ppa:webupd8team/java
+sudo apt install -y  openjdk-8-jdk openjdk-8-jre
 sudo apt-get update
 
 # Set the "don't bother us" option to install.
-echo debconf shared/accepted-oracle-license-v1-1 select true | \
+#echo debconf shared/accepted-oracle-license-v1-1 select true | \
 sudo debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | \
+#echo debconf shared/accepted-oracle-license-v1-1 seen true | \
 sudo debconf-set-selections
 
 # Install Java 8.
-sudo apt-get -y install oracle-java8-installer oracle-java8-set-default
+# sudo apt-get -y install oracle-java8-installer oracle-java8-set-default
