b7a318c4acb94e9b57211be6ce30b7b880f0bba1
vishalnvora <vishalnvora@hotmail.com>
2021-01-05 04:24:37 +0530

    Update install-grafana.sh

--- packer/install-grafana.sh
+++ packer/install-grafana.sh
@@ -3,6 +3,7 @@ set -e
 
 # echo "deb https://packagecloud.io/grafana/stable/debian/ bionic main" | tee -a /etc/apt/sources.list
 # curl https://packagecloud.io/gpg.key | apt-key add -
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 # echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt update && sudo apt install grafana -y
