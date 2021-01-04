f90a54395ed2e68713b4b28dc4d6ed5c5c4e21ce
vishalnvora <vishalnvora@hotmail.com>
2021-01-05 04:05:39 +0530

    Update install-grafana.sh

--- packer/install-grafana.sh
+++ packer/install-grafana.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "deb https://packagecloud.io/grafana/stable/debian/ jessie main" | tee -a /etc/apt/sources.list
+echo "deb https://packagecloud.io/grafana/stable/debian/ bionic main" | tee -a /etc/apt/sources.list
 curl https://packagecloud.io/gpg.key | apt-key add -
 
 sudo apt-get update && sudo apt-get install grafana -y
