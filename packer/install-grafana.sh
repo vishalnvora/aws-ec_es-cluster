257ec8d293bed56d5939bbdbd9d6a056c2e66bb8
vishalnvora <vishalnvora@hotmail.com>
2021-01-05 04:19:50 +0530

    Update install-grafana.sh

--- packer/install-grafana.sh
+++ packer/install-grafana.sh
@@ -6,3 +6,5 @@ set -e
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 # echo "deb https://packages.grafana.com/enterprise/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
 sudo apt update && sudo apt install grafana -y
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
