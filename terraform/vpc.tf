c71f5dd0c55b6991598900d81005d3b931f54eec
vishalnvora <vishalnvora@hotmail.com>
2021-01-05 06:11:07 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,15 @@
+
+variable "vpc_id" {}
+
 data "aws_vpc" "selected" {
-  id = "${var.vpc_id}"
+  id = var.vpc_id
+}  
+data "aws_subnet_ids" "selected" {
+  vpc_id = var.vpc_id
 }
 
-data "aws_subnet_ids" "selected" {
-  vpc_id = "${var.vpc_id}"
+resource "aws_subnet" "example" {
+  vpc_id            = data.aws_vpc.selected.id
+  availability_zone = "us-east-2a"
+  cidr_block        = cidrsubnet(data.aws_vpc.selected.cidr_block, 4, 1)
 }
